c489e687c62d2696883297df4eb4ba155e79c840
mehmetsert <mhmtsrt11@gmail.com>
2021-04-25 18:29:47 +0300

    first commit

---
+++ ckeditor5/build/translations/si.js
@@ -0,0 +1,1 @@
+(function(n){const i=n["si"]=n["si"]||{};i.dictionary=Object.assign(i.dictionary||{},{Bold:"තදකුරු",Italic:"ඇලකුරු",Redo:"නැවත කරන්න",Underline:"",Undo:"අහෝසි කරන්න"});i.getPluralForm=function(n){return n!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ ckeditor5/src/ckeditor.js
@@ -0,0 +1,31 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Bold,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	Italic,
+	Link,
+	Paragraph,
+	Underline
+];
+
+export default Editor;
